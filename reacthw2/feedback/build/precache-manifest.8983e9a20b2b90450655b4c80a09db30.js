4e99f4d462421b74c2c28cc3bb119699b1a5018a
pabadm <balanpasha9@gmail.com>
2020-06-24 02:15:13 +0300

    addedPropTypes1hw

---
+++ reacthw2/feedback/build/precache-manifest.8983e9a20b2b90450655b4c80a09db30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fe3bd7842f7653ee9b1cb9dc62fe49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ecfc5fa0dd151e43a7f",
+    "url": "/static/css/main.f268e409.chunk.css"
+  },
+  {
+    "revision": "fb678a7ea4dcef05223b",
+    "url": "/static/js/2.121816c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.121816c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecfc5fa0dd151e43a7f",
+    "url": "/static/js/main.58481f04.chunk.js"
+  },
+  {
+    "revision": "762eec3fc461a456eed1",
+    "url": "/static/js/runtime-main.3697f3dc.js"
+  }
+]);
