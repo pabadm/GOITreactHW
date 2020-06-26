8ae6784e9e0f97b14929618cd46518fc6c8fb62b
pabadm <balanpasha9@gmail.com>
2020-06-26 22:37:35 +0300

    deleted-old-phonebook-project

--- reacthw2/phonebook/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Phonebook from "./components/Phonebook";
-
-ReactDOM.render(<Phonebook />, document.querySelector("#root"));
