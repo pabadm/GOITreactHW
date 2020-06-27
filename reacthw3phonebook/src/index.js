1ca84672a6ba6d3a56164ec9e8b268db819c1c42
pabadm <balanpasha9@gmail.com>
2020-06-27 19:53:32 +0300

    completed and fixed 2nd homework. added pre 3rd homework

---
+++ reacthw3phonebook/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Phonebook from './components/Phonebook';
+
+ReactDOM.render(<Phonebook />, document.querySelector('#root'));
